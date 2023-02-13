c814844e098b5f5a9e381ab0db014dc206a04511
Oliver Bestwalter <oliver.bestwalter@nortonlifelock.com>
2023-02-13 13:49:13 +0100

    update licens and copyrigh and tidy up

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup
 
 setup(
     name="qox",
-    version="0.1",
+    author="Oliver Bestwalter",
+    copyright="Gen Digital Inc",
+    version="23.2.10",
     packages=["qox_pkg"],
     extras_require={"lint": ["black", "mypy"], "test": ["pytest"]},
     entry_points={"console_scripts": ["qox = qox_pkg.qox:main"]},
